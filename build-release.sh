b27c7ec04e502cf5299e5955cc7f692bdff6123a
chris <chris@weavingwebs.co.uk>
2023-04-18 10:31:26 +0100

    Update build-release.sh to download GeoIP using license

--- build-release.sh
+++ build-release.sh
@@ -6,9 +6,24 @@ set -eux
 ROOT="$(cd "$(dirname "$0")"; pwd -P)"
 cd "$ROOT"
 
-# Download GeoIP DB. @todo this no longer works, you must manually download it.
-#curl -O http://geolite.maxmind.com/download/geoip/database/GeoLite2-Country.mmdb.gz
-#gunzip --force GeoLite2-Country.mmdb.gz
+# Load .env file.
+if [ -f .env ]; then
+  set -o allexport
+  source .env
+  set +o allexport
+fi
+
+# Download GeoIP DB.
+# https://www.maxmind.com/en/account
+TMP_DIR="$(mktemp -d)"
+cd "$TMP_DIR"
+wget -O GeoLite2-Country.mmdb.tar.gz \
+  "https://download.maxmind.com/app/geoip_download?edition_id=GeoLite2-Country&license_key=${MAXMIND_LICENSE_KEY}&suffix=tar.gz"
+tar --wildcards -xzf GeoLite2-Country.mmdb.tar.gz GeoLite2-Country_*/GeoLite2-Country.mmdb
+mv GeoLite2-Country_*/GeoLite2-Country.mmdb "$ROOT"
+rm -rf "$TMP_DIR"
+
+cd "$ROOT"
 
 # Install composer deps.
 #composer install --no-dev
@@ -16,13 +31,13 @@ cd "$ROOT"
 # Build ZIP.
 rm -rf dist/ww_auth_log.zip dist/ww_auth_log
 mkdir -p dist/ww_auth_log
-rsync -r \
-  --delete \
-  --exclude=".idea" \
-  --exclude=".git" \
-  --exclude="build-release.sh" \
-  --exclude="dist" \
-  . \
+cp -rv \
+  src \
+  vendor \
+  wp-ww_auth_log.php \
+  LICENSE \
+  GeoLite2-Country.mmdb \
   dist/ww_auth_log
 cd dist
 zip -r ww_auth_log.zip ww_auth_log
+rm -rf ww_auth_log
